1c6220e6abc07f434693dbedef1dffc469d461b6
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-09 22:19:18 +0200

    Add event listeners to index.js

--- index.js
+++ index.js
@@ -21,3 +21,32 @@ function validateEmail(email) {
     return String(email)
       .toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 };
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = emailinput.value;
+    console.log(email);
+    console.log("hi");
+
+    if (validateEmail(email)) {
+        formSuccess()
+        useremail.innerHTML = email;
+        emailinput.value = "";
+
+        erroremail.classList.remove("active");
+        emailinput.classList.remove("active");
+
+
+    } else {
+        erroremail.classList.add("active");
+        emailinput.classList.add("active");
+        emailinput.style.color = "hsl(4, 100%, 67%)";
+    }
+})
+
+dismissbtn.addEventListener("click", (e) => {
+    leftP.style.display = "block";
+    rightP.style.display = "block";
+    signupform.classList.remove("success");
+    successmsg.classList.remove("active");
+})
